33f1d35e7f5e23e27b35c0ea983a33558f445157
Nikhil6261 <Nikkuahirwar2@gmail.com>
2025-06-28 15:54:50 +0530

    admindash

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AdminDashboard = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold text-blue-700">Admin Dashboard</h1>
+      <h1 className="text-2xl font-bold text-blue-700"> heelooooooooooooo Admin Dashboard</h1>
     </div>
   );
 };
